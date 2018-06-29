88b2c9e1ecc35fc5453619b1a27a570ae6cb9e92
Luiz Carlos Lins Filho <carlosflins@gmail.com>
2018-06-28 22:57:18 -0300

    Added simple-compile-error that shows a compile error trying to associate a value 102.0 to a float variable. And another version compiled with a correct 102.0f value.

---
+++ simple-compile-error/CompatibleNumberTypes.java
@@ -0,0 +1,6 @@
+public class CompatibleNumberTypes {
+	public static void main(String[] args) {
+		float value = 102.0f; // here I specify 'f' of float at end.
+		System.out.println("program runs: " + value);
+	}
+}
---
+++ simple-compile-error/IncompatibleNumberTypes.java
@@ -0,0 +1,5 @@
+public class IncompatibleNumberTypes {
+	public static void main(String[] args) {
+		float value = 102.0; // here are an double value.
+	}
+}
